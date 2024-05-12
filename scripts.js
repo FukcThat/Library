a9b5dfb6dee423633c57f961836ad902f4af1f3c
FukcThat <63652e636f6465@gmail.com>
2024-05-12 23:42:31 +0200

    FEAT: Made Book Object Constructor

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-console.log(5);
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return (
+      this.title +
+      " by " +
+      this.author +
+      ", " +
+      this.pages +
+      " pages, " +
+      (this.readStatus ? "" : "not ") +
+      "read"
+    );
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R.Tolkien", "295", false);
+
+console.log(theHobbit.info());
