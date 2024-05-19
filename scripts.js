6d211830814d6d5cc5c0561545dbd1cbfe501f9a
FukcThat <63652e636f6465@gmail.com>
2024-05-19 17:34:24 +0200

    STYLE: Made form a slidey sidebar with a neat flag button

--- scripts.js
+++ scripts.js
@@ -34,7 +34,10 @@ function displayBooks() {
     newBookDiv.className = "book";
 
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
+    deleteBtn.className = "deleteButton";
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 28 28">
+    <path fill="#EF2828" d="M11.5 6h5a2.5 2.5 0 0 0-5 0M10 6a4 4 0 0 1 8 0h6.25a.75.75 0 0 1 0 1.5h-1.31l-1.217 14.603A4.25 4.25 0 0 1 17.488 26h-6.976a4.25 4.25 0 0 1-4.235-3.897L5.06 7.5H3.75a.75.75 0 0 1 0-1.5zM7.772 21.978a2.75 2.75 0 0 0 2.74 2.522h6.976a2.75 2.75 0 0 0 2.74-2.522L21.436 7.5H6.565zM11.75 11a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-1.5 0v-8.5a.75.75 0 0 1 .75-.75m5.25.75a.75.75 0 0 0-1.5 0v8.5a.75.75 0 0 0 1.5 0z" />
+    </svg>`;
     deleteBtn.addEventListener("click", () => {
       myLibrary.splice(i, 1);
       displayBooks();
@@ -80,10 +83,20 @@ displayBooks();
 
 document.getElementById("showFormBtn").addEventListener("click", function () {
   const form = document.getElementById("addBookForm");
-  if (form.style.display === "none") {
-    form.style.display = "block";
+  const button = document.getElementById("showFormBtn");
+  const header = document.getElementById("Title");
+  const books = document.getElementById("libraryDiv");
+
+  if (form.style.width === "15rem") {
+    form.style.width = "0rem";
+    button.style.left = "0";
+    header.style.left = "15rem";
+    books.style.left = "4rem";
   } else {
-    form.style.display = "none";
+    form.style.width = "15rem";
+    button.style.left = "15rem";
+    header.style.left = "25rem";
+    books.style.left = "20rem";
   }
 });
 
